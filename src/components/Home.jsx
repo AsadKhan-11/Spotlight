a0facca2f6e4bd8be568c28670fa8aec4d0ffa5a
Asad Khan <mrasad10khan@gmail.com>
2025-08-31 18:35:06 +0500

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-[#250000] h-screen ">
+      <div className="container">
+        <h1 className="uppercase text-9xl text-amber-200 font-bold text-center ">
+          Spotlight
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
