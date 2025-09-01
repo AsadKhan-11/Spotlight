d9cc8ce4c69709f6b4775887b399d32a51ce65d6
Asad Khan <mrasad10khan@gmail.com>
2025-09-02 01:43:46 +0500

    Minor Adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,9 +28,9 @@ const Home = () => {
   return (
     <div className="relative text-center  bg-[#FFEDD5] h-screen ">
       <div className="container">
-        <div className="relative pb-20 overflow-hidden w-full max-w-[1024px]  ">
+        <motion.div className=" pb-20 overflow-hidden w-full max-w-[1024px]  ">
           <motion.h1
-            className="relative uppercase mt-10 flex justify-center items-center text-[150px]  transform scale-x-125 scale-y-200 text-[#ec9706] font-bold text-center"
+            className="relative uppercase mt-10 flex justify-center items-center text-[80px] md:text-[120px] lg:text-[150px] transform scale-x-125 scale-y-200 text-[#ec9706] font-bold text-center"
             initial={{ y: "150px" }}
             whileInView={{
               y: "0",
@@ -42,45 +42,47 @@ const Home = () => {
             Spotlight
           </motion.h1>
 
-          {/* Left text animation */}
-          <motion.p
-            className="text-[#ec9706] absolute bottom-0 left-0 text-sm hidden lg:flex"
-            variants={sentence}
-            initial="hidden"
-            whileInView="visible"
-          >
-            {"Your life is your movie, every day a new scene"
-              .split(" ")
-              .map((wordText, index) => (
-                <motion.span
-                  key={index}
-                  variants={word}
-                  className="mr-1 inline-block"
-                >
-                  {wordText}
-                </motion.span>
-              ))}
-          </motion.p>
+          <div className="absolute bottom-0 w-full justify-between text-sm hidden lg:flex  items-end px-6">
+            {/* Left text animation */}
+            <motion.p
+              className="text-[#ec9706]  "
+              variants={sentence}
+              initial="hidden"
+              whileInView="visible"
+            >
+              {"Your life is your movie, every day a new scene"
+                .split(" ")
+                .map((wordText, index) => (
+                  <motion.span
+                    key={index}
+                    variants={word}
+                    className="mr-1 inline-block"
+                  >
+                    {wordText}
+                  </motion.span>
+                ))}
+            </motion.p>
 
-          {/* Right text animation */}
-          <motion.p
-            className="text-[#ec9706] absolute bottom-0 right-0 text-sm flex"
-            variants={sentence}
-            initial="hidden"
-            whileInView="visible"
-          >
-            {"Step up, take the lead, and make it unforgettable."
-              .split(" ")
-              .map((wordText, index) => (
-                <motion.span
-                  key={index}
-                  variants={word}
-                  className="mr-1 inline-block"
-                >
-                  {wordText}
-                </motion.span>
-              ))}
-          </motion.p>
+            {/* Right text animation */}
+            <motion.p
+              className="text-[#ec9706] "
+              variants={sentence}
+              initial="hidden"
+              whileInView="visible"
+            >
+              {"Step up, take the lead, and make it unforgettable."
+                .split(" ")
+                .map((wordText, index) => (
+                  <motion.span
+                    key={index}
+                    variants={word}
+                    className="mr-1 inline-block"
+                  >
+                    {wordText}
+                  </motion.span>
+                ))}
+            </motion.p>
+          </div>
           <motion.div
             initial={{ opacity: 0, scale: 0, rotate: 90 }}
             animate={{ rotate: 0, opacity: 1, scale: 1 }}
@@ -89,7 +91,7 @@ const Home = () => {
           >
             <IoStarSharp className="h-full w-full text-[#ed820e] stroke-10 stroke-[#1d4557] " />
           </motion.div>
-        </div>
+        </motion.div>
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/4 z-50">
           <motion.img
             src={model}
@@ -100,30 +102,35 @@ const Home = () => {
             transition={{ duration: 0.5, ease: "easeInOut", delay: 1.5 }}
           />
         </div>
+      </div>
+      <motion.div className="absolute z-60 bottom-0 w-full flex justify-between items-end p-6">
+        {/* Icons group (can overlap) */}
         <motion.div
-          className=" flex absolute bottom-0 left-7 z-60  "
+          className="flex -space-x-4"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 2, duration: 0.5 }}
         >
-          <IoGlobeOutline className="h-10 w-10 text-[#ec9706] " />
+          <IoGlobeOutline className="h-10 w-10 text-[#ec9706]" />
           <IoGlobeOutline className="h-10 w-10 text-[#ec9706]" />
         </motion.div>
+
+        {/* Right restricted box */}
         <motion.div
-          className="absolute text-[#ec9706] bottom-0 right-7 z-60 grid grid-cols-2 border-2 divide-x w-64 h-20 text-xs"
+          className=" text-[#ec9706] grid grid-cols-[60px_1fr]  border-2 max-w-[200px] h-20 text-xs"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 2, duration: 0.5 }}
         >
-          <h3 className="row-span-2 flex items-center justify-center text-4xl font-extrabold">
+          <h3 className="row-span-2 flex items-center border-r-2 border-r-[#ec9706] justify-center text-4xl font-extrabold">
             R
           </h3>
-          <h3 className="border-b font-semibold text-sm">Restricted</h3>
+          <h3 className="border-b font-semibold text-sm">Restricted</h3>{" "}
           <p className="leading-tight">
-            Under 17 requires accompanying parent or adult guardian.
+            Under 17 requires accompanying parent or adult guardian.{" "}
           </p>
         </motion.div>
-      </div>
+      </motion.div>
       <motion.div
         className="bg-[#2C5364] h-[200px] w-full absolute bottom-0 z-0"
         initial={{ width: "0%" }}
